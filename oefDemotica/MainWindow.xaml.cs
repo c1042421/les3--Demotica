fd2f2a32e431316b0a24d548d18b21187384f3f0
c1042421@hik.be <c1042421@hik.be>
2017-10-10 00:39:11 +0200

    added ui and logic behind ui

--- oefDemotica/MainWindow.xaml.cs
+++ oefDemotica/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Controls.Primitives;
 
 namespace oefDemotica
 {
@@ -23,6 +24,101 @@ namespace oefDemotica
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        Lichten lamp;
+        Verwarming verwarming;
+        PLC plc;
+
+        private void ToggleButton_Checked(object sender, RoutedEventArgs e)
+        {
+            ToggleButton toggleButton = (ToggleButton)sender;
+            bool btnChecked = toggleButton.IsChecked ?? false;
+
+            switch (toggleButton.Name)
+            {
+                case "btnLamp":
+                    lamp.Power = btnChecked;
+                    break;
+                case "btnDomoLamp":
+                    if (btnChecked)
+                    {
+                        plc.DoeLichtenAan();
+                    }
+                    else
+                    {
+                        Valloirplc.DoeLichtenUit();
+                    }
+                    break;
+                case "btnVerwarming":
+                    if (btnChecked)
+                    {
+                        plc.ZetVerwarmingOp();
+                    }
+                    else
+                    {
+                        plc.ZetVerwarmingAf();
+                    }
+                    break;
+            }
+
+            updateView();
+        }
+
+        private void updateView()
+        {
+            updateGraden();
+            updateImage();
+        }
+
+        private void updateGraden()
+        {
+            lblCelcius.Content = plc.Chauffage.Graden;
+            lblFahrenheit.Content = plc.Chauffage.InFahrenheit();
+        }
+
+        private void updateImage()
+        {
+            bool lampAan = plc.SalonLichten.Power;
+
+            string aan = "images/aan.png";
+            string uit = "images/uit.gif";
+
+            BitmapImage source;
+
+            if (lampAan) source = MakeBitmapFor(aan);
+            else source = MakeBitmapFor(uit);
+
+            imgLamp.Source = source;
+        }
+
+        private BitmapImage MakeBitmapFor(string url)
+        {
+            Uri uri = new Uri(url, UriKind.Relative);
+            return new BitmapImage(uri);
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            lamp = new Lichten();
+            verwarming = new Verwarming();
+            plc = new PLC();
+
+            plc.Chauffage = verwarming;
+            plc.SalonLichten = lamp;
+
+            updateView();
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            Slider slider = (Slider)sender;
+
+            int graden = (int) slider.Value;
+
+            plc.PasTemperatuurAan(graden);
+
+            updateView();
         }
     }
 }
