3199f9405a3bb1249f790286dd683477b86cc5e7
c1042421@hik.be <c1042421@hik.be>
2017-10-09 19:25:32 +0200

    added logic

---
+++ oefDemotica/PLC.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oefDemotica
+{
+    class PLC
+    {
+        public Verwarming Chauffage { get; set; }
+        public Lichten SalonLichten { get; set; }
+
+        public void DoeLichtenAan()
+        {
+            SalonLichten.Power = true;
+        }
+
+        public void DoeLichtenUit()
+        {
+            SalonLichten.Power = false;
+        }
+
+        public void PasTemperatuurAan(double graden)
+        {
+            Chauffage.Graden = graden;
+        }
+
+        public void ZetVerwarmingAf()
+        {
+            Chauffage.Power = false;
+        }
+
+        public void ZetVerwarmingOp()
+        {
+            Chauffage.Power = true;
+        }
+    }
+}
---
+++ oefDemotica/Verwarming.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oefDemotica
+{
+    class Verwarming
+    {
+        private double graden;
+
+        public bool Power { get; set; }
+        public double Graden { get => graden;
+            set {
+                if (Power) graden = value;
+            }
+        }
+
+        public double InFahrenheit()
+        {
+            return Graden * 9 / 5 + 32;
+        }
+    }
+}
